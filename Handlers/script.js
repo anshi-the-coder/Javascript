74f5398bab40cfe592417055e8e0a00c935288ff
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-10 11:06:47 +0530

    problem handlers

---
+++ Handlers/script.js
@@ -0,0 +1,21 @@
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+       // alert("Hey I am not resolved")
+        resolve(1);
+    },2000)
+})
+p1.then(()=>{
+    console.log("Hurray")
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(4) 
+        },6000)
+
+    }).then((value)=>{
+        console.log(value)
+    })
+})
+
+p1.then(()=>{
+   console.log("Congratulations this promise is now resolved ")
+})
