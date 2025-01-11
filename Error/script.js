32ff1f64362f154cfba8849c59a908c43f6ae1b6
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-11 13:12:03 +0530

    Error Handling

---
+++ Error/script.js
@@ -0,0 +1,24 @@
+setTimeout(()=>{
+    console.log("Hacking wifi....Please wait....")
+}, 1000)
+
+try{  // Whenever we want to executes some code or test some code.
+      setTimeout(()=>{
+            console.log(Rahul)
+      }, 100)
+}
+catch(error){         // catch block executes when any error occurs in the try block.
+    console.log("Balle balle")
+}
+setTimeout(()=>{
+    console.log("Fetching username and password....Please wait....")
+}, 2000)
+
+setTimeout(()=>{
+    console.log("Hacking Rahul's faebook id....Please wait....")
+}, 3000)
+
+setTimeout(()=>{
+    console.log("username and password of Rahul(+919639628931) fetched....Please wait....")
+}, 4000) 
+
