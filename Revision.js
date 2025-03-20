6ee29470805890de45a1f2e52bfd4feb7aa196de
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-03-20 21:16:06 +0530

    Revision of js

--- Revision.js
+++ Revision.js
@@ -1,5 +1,10 @@
 let a =[2,1,5,3,4]
-let arr=a.filter((value)=>{
-return value%2==0
+// let arr=a.filter((value)=>{
+// return value%2==0
+// })
+// console.log(arr)
+
+let arr = a.map((value,index)=>{
+    return value*index
 })
 console.log(arr)
