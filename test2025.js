6b7880255392b72747d4913a1823446274988c2f
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-05-10 11:24:39 +0530

    Revision Map Filter and reduce

--- test2025.js
+++ test2025.js
@@ -1,11 +1,30 @@
-let arr=[2,9,100,1000,8,5,3,4]
-let a=arr.filter((value)=>{
- return value%2==0
-})
-// a.sort((a,b)=>(a-b))
+// let arr=[2,9,100,1000,8,5,3,4]
+// let a=arr.filter((value)=>{
+//  return value%2==0
+// })
+// // a.sort((a,b)=>(a-b))
+// console.log(a)
+
+// filter
+// let ar = [2,3,43,21,44,56]
+// let a1 = ar.filter((value)=>{
+//     return value%2==0
+// })
+// console.log(a1)
+
+// reduce
+// let a = [3,4,2,45,42,12]
+// let a2 = a.map((value)=>{
+//     return value*value
+// })
+// console.log(a2)
+
+// reduce
+let a = [2,1,3,43,4,5]
+let a1 = ((h1,h2)=>{
+    return h1+h2
+}) 
 console.log(a)
-
-
 
 
 
